3321e8eeda271aa533e931b7f93b7e4a66a80265
UP210170 <up210170@alumnos.upa.edu.mx>
2024-03-31 20:07:20 -0600

    Subiendo el contador de click

---
+++ Cont_Clicks/app.js
@@ -0,0 +1,49 @@
+let containerClicks = document.getElementById('container-clicks');
+
+let btnIncrement = document.querySelector('.btn-primary');
+let btnDecrement = document.querySelector('.btn-secondary');
+let btnReset = document.querySelector('.btn-reset');
+
+let countClicks = 0;
+
+document.addEventListener('DOMContentLoaded', () => {
+    containerClicks.innerText = countClicks;
+
+    btnIncrement.addEventListener("click", function(){
+        //alert("Le diste click al boton");
+        countClicks++;
+        containerClicks.innerText = countClicks;
+    });
+
+    btnDecrement.onclick = function(){
+        if(countClicks > 0){
+            countClicks--;
+            containerClicks.innerText = countClicks;
+        }else{
+            alert("El contador esta en 0");
+        }  
+    }
+    
+    btnReset.addEventListener("click", function(){
+        countClicks = 0;
+        containerClicks.innerText = countClicks;
+    });
+});
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//document.getElementById('container-clicks').style.backgroundColor = "#000";
+//containerClicks.style.color = "#fff";
